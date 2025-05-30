cae0dc5f24274f56a7e5d13c86565cb23355a252
caspKth <kethelynaj@hotmail.com>
2025-05-30 04:36:43 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#top-right").on("mouseenter", function(){
+        $("#white").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#top-right").on("mouseout", function(){
+        $("#white").hide();
+    });
+});
+
+ $(document).ready(function(){
+    $("#bottom-left").on("mouseenter", function(){
+        $("#milk").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#bottom-left").on("mouseout", function(){
+        $("#milk").hide();
+    });
+});
+
+ $(document).ready(function(){
+    $("#bottom-right").on("mouseenter", function(){
+        $("#truffle").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#bottom-right").on("mouseout", function(){
+        $("#truffle").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#top-left").on("mouseenter", function(){
+        $("#truffle2").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#top-left").on("mouseout", function(){
+        $("#truffle2").hide();
+    });
+});
